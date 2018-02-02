c2ee2870f41eae149524c1948c09ca2fb17885c1
Elena Mikhailova <kyznezhik@mail.ru>
2018-02-02 15:55:48 +0300

    добавлены функции

--- sandbox/src/main/java/lanit/qa/firstjava/MyFirstProgram.java
+++ sandbox/src/main/java/lanit/qa/firstjava/MyFirstProgram.java
@@ -3,7 +3,27 @@ package lanit.qa.firstjava;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        func("world");
+        func("user");
+        func("Lena");
+
+        double m = 6;
+        System.out.println("Площадь квадрата со стороной " + m + "=" + area(m));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + "=" + area(a, b));
     }
 
-}
+    public static void func(String val) {
+        System.out.println("Hello, " + val + "!");
+    }
+
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+    return a * b;
+    }
+    }
